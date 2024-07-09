04f7edb4a46dff3c6c0fb20a1d1d0a71920941b0
jonayeddewan <2010006@student.ruet.ac.bd>
2024-07-10 00:26:51 +0600

    tutorial problems

--- Tutorial.js
+++ Tutorial.js
@@ -1,15 +1,23 @@
-//grade according to marks
-let score = prompt("Enter Your Marks: ");
-if (score <= 100 && score >= 80) {
-    console.log("Grade A");
-} else if (score <= 79 && score >= 70) {
-    console.log("Grade B");
-} else if (score <= 69 && score >= 60) {
-    console.log("Grade C");
-} else if (score <= 59 && score >= 50) {
-    console.log("Grade D");
-} else if (score <= 49 && score >= 0) {
-    console.log("Grade F");
-} else {
-    console.log("Invalid input");
+// //grade according to marks
+// let score = prompt("Enter Your Marks: ");
+// if (score <= 100 && score >= 80) {
+//     console.log("Grade A");
+// } else if (score <= 79 && score >= 70) {
+//     console.log("Grade B");
+// } else if (score <= 69 && score >= 60) {
+//     console.log("Grade C");
+// } else if (score <= 59 && score >= 50) {
+//     console.log("Grade D");
+// } else if (score <= 49 && score >= 0) {
+//     console.log("Grade F");
+// } else {
+//     console.log("Invalid input");
+// }
+
+// Print all even number from 0 to 100
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
 }
